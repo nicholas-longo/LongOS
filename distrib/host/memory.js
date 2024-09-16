3de5ed3f6ee1dd115b389c973453a6059b0f6bc1
nicholas-longo <nlongo1004@gmail.com>
2024-09-16 16:03:52 -0400

    Issue 2.0.1 Added in the beginning of the memory class and made it so the array gets created

--- distrib/host/memory.js
+++ distrib/host/memory.js
@@ -1,9 +1,40 @@
+/**
+ * Some of this content was taken from my Org and Arch Project
+ */
 var TSOS;
 (function (TSOS) {
     class Memory {
+        memory;
+        MAR; // memory address register
+        MDR; // memory data register
         constructor() {
+            ;
+            //initializes the memory array
+            this.memory = [];
+            this.MAR = 0;
+            this.MDR = 0;
+            this.init();
         }
+        // prob will change the number of bytes the memory array is
+        // initialize a blank memory array. the size will be able to store three programs that are 256 bytes 
         init() {
+            for (let i = 0x00; i < 0x300; i += 0x01) {
+                this.memory[i] = 0x00;
+            }
+        }
+        getMDR() {
+            return this.MDR;
+        }
+        setMDR(value) {
+            this.MDR = value;
+        }
+        // reads the memory location in the MAR and updates the MDR
+        read() {
+            this.MDR = this.memory[this.MAR];
+        }
+        // write the contents of the MDR to memory at the location indicated by the MAR
+        write() {
+            this.memory[this.MAR] = this.MDR;
         }
     }
     TSOS.Memory = Memory;
--- source/host/memory.ts
+++ source/host/memory.ts
@@ -1,13 +1,46 @@
+/**
+ * Some of this content was taken from my Org and Arch Project 
+ */
+
 module TSOS {
 
     export class Memory {
+        private memory: number[]; 
+        private MAR: number; // memory address register
+        private MDR: number; // memory data register
 
-        constructor() {
-
-        }
+         constructor() {;
+        //initializes the memory array
+        this.memory = [];
+        this.MAR = 0;
+        this.MDR = 0;
+        this.init();
+    }
         
+    // prob will change the number of bytes the memory array is
+    // initialize a blank memory array. the size will be able to store three programs that are 256 bytes 
         public init(): void {
-            
+            for (let i: number = 0x00; i < 0x300; i += 0x01) {
+                this.memory[i] = 0x00; 
+            }
+        }
+
+        public getMDR(): number {
+            return this.MDR;
+        }
+    
+        public setMDR(value: number): void {
+            this.MDR = value;
+        }
+    
+        // reads the memory location in the MAR and updates the MDR
+        public read(): void {
+            this.MDR = this.memory[this.MAR];
+        }
+    
+        // write the contents of the MDR to memory at the location indicated by the MAR
+        public write(): void {
+            this.memory[this.MAR] = this.MDR;
         }
     }
 }
