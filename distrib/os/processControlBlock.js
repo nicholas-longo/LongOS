7ae8736b1367ae5be78d2077002fb221032cf28a
nicholas-longo <nlongo1004@gmail.com>
2024-09-17 19:37:05 -0400

    prepping pcb and the pcb / cpu html

--- distrib/os/processControlBlock.js
+++ distrib/os/processControlBlock.js
@@ -1,11 +1,36 @@
-// TODO 
-// ask about this implementation for the iProject2
 var TSOS;
 (function (TSOS) {
     class ProcessControlBlock {
-        constructor() {
+        PID;
+        priority;
+        location;
+        PC;
+        acc;
+        xReg;
+        yReg;
+        zFlag;
+        Status;
+        constructor(PID = 0, priority = 0, location = "", PC = 0, acc = 0, xReg = 0, yReg = 0, zFlag = 0, Status = "") {
+            this.PID = PID;
+            this.priority = priority;
+            this.location = location;
+            this.PC = PC;
+            this.acc = acc;
+            this.xReg = xReg;
+            this.yReg = yReg;
+            this.zFlag = zFlag;
+            this.Status = Status;
         }
         init() {
+            this.PID = 0;
+            this.priority = 0;
+            this.location = "";
+            this.PC = 0;
+            this.acc = 0;
+            this.xReg = 0;
+            this.yReg = 0;
+            this.zFlag = 0;
+            this.Status = "";
         }
     }
     TSOS.ProcessControlBlock = ProcessControlBlock;
--- source/os/processControlBlock.ts
+++ source/os/processControlBlock.ts
@@ -1,15 +1,29 @@
-// TODO 
-// ask about this implementation for the iProject2
-
 module TSOS {
 
     export class ProcessControlBlock {
-
-        constructor() {
-
+        constructor ( 
+            public PID: number = 0, 
+            public priority: number = 0,
+            public location: string = "",
+            public PC: number = 0,
+            public acc: number = 0,
+            public xReg: number = 0,
+            public yReg: number = 0,
+            public zFlag: number = 0,
+            public Status: string = "") {
+                    
         }
         
         public init(): void {
+            this.PID = 0; 
+            this.priority = 0; 
+            this.location = "";
+            this.PC = 0;
+            this.acc = 0;
+            this.xReg = 0;
+            this.yReg = 0;
+            this.zFlag = 0;
+            this.Status = "";
         }
     }
 }
