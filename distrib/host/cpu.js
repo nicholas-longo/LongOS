8c5658d76c354bc8be3a23edcf2526d22e0a670a
nicholas-longo <nlongo1004@gmail.com>
2024-09-24 16:51:44 -0400

    Issue 2.0.0: added the fetch step, working through the intitial pipeline. going to have 2 decode and 2 execute steps

--- distrib/host/cpu.js
+++ distrib/host/cpu.js
@@ -14,13 +14,19 @@ var TSOS;
 (function (TSOS) {
     class Cpu {
         PC;
+        IR;
         Acc;
         Xreg;
         Yreg;
         Zflag;
         isExecuting;
-        constructor(PC = 0x00, Acc = 0x00, Xreg = 0x00, Yreg = 0x00, Zflag = 0x00, isExecuting = false) {
+        //used to help with decodes for the String FF call
+        decodeFlag = 0;
+        //used to help with decodes for the String FF call
+        executeFlag = 0;
+        constructor(PC = 0x00, IR = 0x00, Acc = 0x00, Xreg = 0x00, Yreg = 0x00, Zflag = 0x00, isExecuting = false) {
             this.PC = PC;
+            this.IR = IR;
             this.Acc = Acc;
             this.Xreg = Xreg;
             this.Yreg = Yreg;
@@ -29,6 +35,7 @@ var TSOS;
         }
         init() {
             this.PC = 0x00;
+            this.IR = 0x00;
             this.Acc = 0x00;
             this.Xreg = 0x00;
             this.Yreg = 0x00;
@@ -38,13 +45,31 @@ var TSOS;
         cycle() {
             _Kernel.krnTrace('CPU cycle');
             // TODO: Accumulate CPU usage and profiling statistics here.
-            // Do the real work here. Be sure to set this.isExecuting appropriately.\
+            // Do the real work here. Be sure to set this.isExecuting appropriately.
+            this.fetch();
+            this.decode1();
+            this.decode2();
+            this.execute1();
+            this.execute2();
         }
         fetch() {
+            // set MAR to current value of program counter
+            _MemoryAccessor.setMAR(this.PC);
+            //set the MDR to the value of what was at memory[MAR]
+            _MemoryAccessor.read();
+            //get the MDR and set it to the Instruction Register
+            let mdrValue = _MemoryAccessor.getMDR();
+            this.IR = mdrValue;
+            // increment the program counter to the next address because the current memory address was read
+            this.PC += 0x0001;
         }
-        decode() {
+        decode1() {
         }
-        execute() {
+        decode2() {
+        }
+        execute1() {
+        }
+        execute2() {
         }
     }
     TSOS.Cpu = Cpu;
--- source/host/cpu.ts
+++ source/host/cpu.ts
@@ -15,7 +15,15 @@ module TSOS {
 
     export class Cpu {
 
+         //used to help with decodes for the String FF call
+        private decodeFlag: number = 0; 
+
+        //used to help with decodes for the String FF call
+        private executeFlag: number = 0; 
+
+
         constructor(public PC: number = 0x00,
+                    public IR: number = 0x00,
                     public Acc: number = 0x00,
                     public Xreg: number = 0x00,
                     public Yreg: number = 0x00,
@@ -26,6 +34,7 @@ module TSOS {
 
         public init(): void {
             this.PC = 0x00;
+            this.IR = 0x00;
             this.Acc = 0x00;
             this.Xreg = 0x00;
             this.Yreg = 0x00;
@@ -36,19 +45,44 @@ module TSOS {
         public cycle(): void {
             _Kernel.krnTrace('CPU cycle');
             // TODO: Accumulate CPU usage and profiling statistics here.
-            // Do the real work here. Be sure to set this.isExecuting appropriately.\
-            
+            // Do the real work here. Be sure to set this.isExecuting appropriately.
+            this.fetch();
+            this.decode1();
+            this.decode2();
+            this.execute1();
+            this.execute2();
         }
+
 
         public fetch(): void {
+            // set MAR to current value of program counter
+            _MemoryAccessor.setMAR(this.PC);
+
+            //set the MDR to the value of what was at memory[MAR]
+            _MemoryAccessor.read();
+
+             //get the MDR and set it to the Instruction Register
+            let mdrValue = _MemoryAccessor.getMDR()
+            this.IR = mdrValue;
+
+            // increment the program counter to the next address because the current memory address was read
+            this.PC += 0x0001;
 
         }
 
-        public decode(): void {
+        public decode1(): void {
             
         }
 
-        public execute(): void {
+        public decode2(): void {
+            
+        }
+
+        public execute1(): void {
+            
+        }
+
+        public execute2(): void {
             
         }
 
