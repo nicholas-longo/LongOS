3ab2b16af54ae66fc10c00c2a65e99bc6f3702e8
nicholas-longo <nlongo1004@gmail.com>
2024-12-19 14:04:45 -0500

    css overhaul, font color in console changed to white

--- distrib/os/canvastext.js
+++ distrib/os/canvastext.js
@@ -149,7 +149,7 @@ var TSOS;
             ctx.save();
             ctx.lineCap = "round";
             ctx.lineWidth = 2.0 * mag;
-            ctx.strokeStyle = "black";
+            ctx.strokeStyle = "white";
             for (var i = 0; i < len; i++) {
                 var c = CanvasTextFunctions.letter(str.charAt(i));
                 if (!c) {
